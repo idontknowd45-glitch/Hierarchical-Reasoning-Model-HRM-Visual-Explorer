24ce1a03d4485d4c4f8d572a19d7c348f731766c
idontknowd45-glitch <idontknowd45@gmail.com>
2025-12-02 08:07:40 +0000

    Update Header.tsx

--- Header.tsx
+++ Header.tsx
@@ -7,6 +7,7 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
+import ReadmeDialog from './ReadmeDialog';
 
 export default function Header() {
   const { setTheme } = useTheme();
